1b33ae57fb97580c4cabe4cce4a9f76f439bf67f
robbie6153-cmd <robbie6153@icloud.com>
2026-09-05 07:16:08 +0100

    Add files via upload

--- results.js
+++ results.js
@@ -331,6 +331,123 @@ export const resultsByRound = {
   awayScore: 1
 }
 
-  }
+  },
 
+  /* ============================
+     ENGLISH LEAGUE WEEK FOUR
+  ============================ */
+
+  "English League Week Four": {
+
+    1: {
+      homeScore: 0,
+      awayScore: 2
+    },
+
+    2: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    3: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    4: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    5: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    6: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    7: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    8: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    9: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    10: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    11: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    12: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    13: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    14: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    15: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    16: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    17: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    18: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    19: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    20: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    21: {
+      homeScore: null,
+      awayScore: null
+    },
+
+    22: {
+      homeScore: null,
+      awayScore: null
+    }
+
+  }
 };
